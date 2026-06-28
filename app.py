e3cf62b3137ef21b8e95df7cb7f732ca26b20609
atouloupis <atouloupis@yahoo.Fr>
2026-06-28 21:37:12 +0200

    Update app to run on configurable port
    Change the port configuration to use an environment variable.

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os 
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,4 +9,6 @@ def hello():
     return 'Hello Moto! '
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+
+# au lieu de : app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 3000))) 
