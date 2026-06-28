287bdcb8028d0ed44a8eb017954de0303d392a82
atouloupis <atouloupis@yahoo.Fr>
2026-06-28 21:40:52 +0200

    Change default port to 80 for app.run

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def hello():
 if __name__ == '__main__':
 
 # au lieu de : app.run(host="0.0.0.0", port=80)
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 3000))) 
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 80))) 
